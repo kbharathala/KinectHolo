de4a18df8e9434098f3047761782afe6b6b09f36
kbharathala <kbharathala@gmail.com>
2017-07-29 12:42:08 -0700

    camera layer

--- HoloKinect/HoloKinect/VideoPlayerViewController.m
+++ HoloKinect/HoloKinect/VideoPlayerViewController.m
@@ -7,34 +7,74 @@
 //
 
 #import "VideoPlayerViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface VideoPlayerViewController ()
+
+@property (nonatomic, strong) AVCaptureSession *captureSession;
+@property (nonatomic, strong) AVCaptureStillImageOutput *stillImageOuput;
+@property (nonatomic, strong) AVCaptureVideoPreviewLayer *previewLayer;
 
 @end
 
 @implementation VideoPlayerViewController
 
+- (void) viewWillAppear:(BOOL)animated {
+    
+    [super viewWillAppear:animated];
+    
+    // Setting up the navigation bar
+    [self.navigationController.navigationBar setHidden:YES];
+    self.navigationController.navigationItem.backBarButtonItem = nil;
+    
+    // Setting up the camera. I don't remember how this works
+    // wrote this a year ago. @kbharathala
+    self.captureSession = [[AVCaptureSession alloc] init];
+    [self.captureSession setSessionPreset:AVCaptureSessionPresetPhoto];
+    
+    AVCaptureDevice *inputDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    NSError *error;
+    
+    AVCaptureDeviceInput *deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:inputDevice error:&error];
+    
+    if([self.captureSession canAddInput:deviceInput]) {
+        [self.captureSession addInput:deviceInput];
+    }
+    
+    self.previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.captureSession];
+    [self.previewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
+    CALayer *rootLayer = [self.view layer];
+    [rootLayer setMasksToBounds:YES];
+    
+    [self.previewLayer setFrame:self.view.bounds];
+    [rootLayer insertSublayer:self.previewLayer atIndex:0];
+    
+    self.stillImageOuput = [[AVCaptureStillImageOutput alloc] init];
+    NSDictionary *outputSettings = [[NSDictionary alloc] initWithObjectsAndKeys:AVVideoCodecJPEG, AVVideoCodecKey, nil];
+    [self.stillImageOuput setOutputSettings:outputSettings];
+    
+    [self.captureSession addOutput: self.stillImageOuput];
+    [self.captureSession startRunning];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor clearColor];
     
-    
-    self.view.backgroundColor = [UIColor redColor];
+    UISwipeGestureRecognizer *rightSwipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(didSwipe:)];
+    rightSwipe.direction = UISwipeGestureRecognizerDirectionRight;
+    [self.view addGestureRecognizer:rightSwipe];
     
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)didSwipe:(UISwipeGestureRecognizer*) swipe {
+    
+    if (swipe.direction == UISwipeGestureRecognizerDirectionRight) {
+        [self.navigationController popViewControllerAnimated:YES];
+    }
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+
 
 @end
